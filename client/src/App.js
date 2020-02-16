fd420d4ae3a443e470d3fa428e2b36061244ffb0
David <dhahn123@gmail.com>
2020-02-16 13:44:09 -0800

    setup Navbar, Landing components with css file

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
 
-function App() {
-  return (
+
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fragment>
+        <h1>Social Network</h1>
+        <Navbar />
+        <Landing />
+      </Fragment>
     </div>
-  );
-}
+)
+
 
 export default App;
