d99699b792e5684b066e2cc73f288d2f0f0f53b4
David <dhahn123@gmail.com>
2020-02-17 17:17:13 -0800

    Create Profile Actions, Save data to DB

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// import { setAlert } from './alert'
+import { setAlert } from './alert';
 import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 // Get current user's profile
@@ -12,6 +12,40 @@ export const getCurrentProfile = () => async dispatch => {
             payload: res.data
         })
     } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.data.msg, status: error.response.status }
+        });
+    }
+}
+
+// Create or Update Profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const res = await axios.post('/api/profile', formData, config);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'), 'success');
+
+        if(!edit) {
+            history.push('/dashboard');
+        }
+    } catch (error) {
+        const errors = error.response.data.errors;
+
+        if(error){
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
         dispatch({
             type: PROFILE_ERROR,
             payload: { msg: error.response.data.msg, status: error.response.status }
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
+import PropTypes from 'prop-types';
 import React, { Fragment, useState } from 'react';
-// import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
     const [formData, setFormData] = useState({
         company: '',
         website: '',
@@ -35,7 +37,12 @@ const CreateProfile = props => {
         instagram
     } = formData;
 
-    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value })
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData, history)
+    }
 
     return (
         <Fragment>
@@ -47,7 +54,7 @@ const CreateProfile = props => {
             profile stand out
         </p>
         <small>* = required field</small>
-        <form className="form">
+        <form className="form" onSubmit={e => onSubmit(e)}>
             <div className="form-group">
             <select name="status" value={status} onChange={e => onChange(e)}>
                 <option value="0">* Select Professional Status</option>
@@ -115,7 +122,7 @@ const CreateProfile = props => {
             <span>Optional</span>
             </div>
 
-            {displaySocialInputs && <Fragment>
+            {displaySocialInputs && (<Fragment>
                 <div className="form-group social-input">
                 <i className="fab fa-twitter fa-2x"></i>
                 <input type="text" placeholder="Twitter URL" name="twitter" value={twitter} onChange={e => onChange(e)} />
@@ -140,7 +147,7 @@ const CreateProfile = props => {
                 <i className="fab fa-instagram fa-2x"></i>
                 <input type="text" placeholder="Instagram URL" name="instagram" value={instagram} onChange={e => onChange(e)}  />
                 </div>
-                </Fragment>}
+                </Fragment>)}
 
             <input type="submit" className="btn btn-primary my-1" />
             <a className="btn btn-light my-1" href="dashboard.html">Go Back</a>
@@ -150,8 +157,7 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+    createProfile: PropTypes.func.isRequired,
 }
 
-
-export default connect()(CreateProfile);
+export default connect(null, {createProfile})(withRouter(CreateProfile));
