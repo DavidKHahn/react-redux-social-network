581598503aac0a3f971f819dba565418bcd0e468
David <dhahn123@gmail.com>
2020-02-19 11:42:47 -0800

    Add/Unlike functionality: Actions, Reducers

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,8 +3,9 @@ import React from 'react';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { addLike, removeLike } from '../../actions/post';
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date } }) =>
+const PostItem = ({ addLike, removeLike, auth, post: { _id, text, name, avatar, user, likes, comments, date } }) =>
         <div className="post bg-white p-1 my-1">
         <div>
           <a href="profile.html">
@@ -23,12 +24,12 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
            <p className="post-date">
             Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
           </p>
-          <button type="button" className="btn btn-light">
+          <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
             <i className="fas fa-thumbs-up"></i>{' '}
             <span>{likes.length > 0 && (<span>{likes.length}</span>)}</span>
 
           </button>
-          <button type="button" className="btn btn-light">
+          <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
             <i className="fas fa-thumbs-down"></i>
           </button>
 
@@ -52,11 +53,13 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
 
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired
+    auth: PropTypes.object.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps, {  })(PostItem)
+export default connect(mapStateToProps, {  addLike, removeLike })(PostItem)
