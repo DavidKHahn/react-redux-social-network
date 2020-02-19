6b54c14c3de791e517bffb2774d4b1a10d33e9a7
David <dhahn123@gmail.com>
2020-02-18 22:21:57 -0800

    Create ProfileGithub Component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import Spinner from '../layout/Spinner';
 import ProfileAbout from './ProfileAbout';
 import ProfileEducation from './ProfileEducation';
 import ProfileExperience from './ProfileExperience';
+import ProfileGithub from './ProfileGithub';
 import ProfileTop from './ProfileTop';
 
 const Profile = ({ match, getProfileById, profile: { profile, loading }, auth }) => {
@@ -47,6 +48,9 @@ const Profile = ({ match, getProfileById, profile: { profile, loading }, auth })
                             </Fragment>) : (<h4>No Education credentials</h4>)}
                         </div>
 
+                    {profile.githubusername && (
+                        <ProfileGithub username={profile.githubusername} />
+                    )}
                     </div>
             </Fragment>}
         </Fragment>
